9b060480ac0154185a94fedc3a5a6b8f311874e4
Sahil Shirpure <nayanshirpure7@gmail.com>
2025-09-22 09:09:25 +0000

    > Build failed because of webpack errors Error: Command "npm run build"

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,2 +1,14 @@
 import { config } from 'dotenv';
-config();
+import { genkit } from 'genkit';
+import { googleAI } from '@genkit-ai/googleai';
+
+config({ path: '.env.local' });
+
+genkit({
+  plugins: [googleAI()],
+  // Must be in a client-facing file, so it can be invoked by the browser.
+  flows: [],
+
+  // Genkit-nextjs plugin requires this to be set.
+  dotEnvPath: '.env.local',
+});
--- src/app/api/genkit/[slug]/route.ts
+++ src/app/api/genkit/[slug]/route.ts
@@ -1,17 +1,4 @@
-'use client';
 import { defineNextJsHandler } from '@genkit-ai/next';
-import {googleAI} from '@genkit-ai/googleai';
-import {genkit} from 'genkit';
-import { z } from 'zod';
+import '@/ai/dev';
 
-genkit({
-  plugins: [googleAI()],
-  // Must be in a client-facing file, so it can be invoked by the browser.
-  flows: [],
-
-  // Genkit-nextjs plugin requires this to be set.
-  dotEnvPath: '.env.local',
-});
-
-
-export const {GET, POST} = defineNextJsHandler();
+export const { GET, POST } = defineNextJsHandler();
